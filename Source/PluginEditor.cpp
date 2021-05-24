f85afd99d1a4e1017ec09d1ecadff8dcf6bb0eb6
Janith Harinda <jharinda@gmail.com>
2021-05-24 11:46:11 +0530

    Filter Drawm

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,20 @@ FIRFilterAudioProcessorEditor::FIRFilterAudioProcessorEditor (FIRFilterAudioProc
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+
+    filterCutoffDial.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    filterCutoffDial.setRange(audioProcessor.minFilterCutoff, audioProcessor.maxFilterCutoff);
+    filterCutoffDial.setValue(audioProcessor.defaultFilterCutoff);
+    filterCutoffDial.setTextValueSuffix("hz");
+    filterCutoffDial.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+
+    addAndMakeVisible(&filterCutoffDial);
+
+
+    filterCutoffValue = new juce::AudioProcessorValueTreeState::SliderAttachment
+    (audioProcessor.treeState, audioProcessor.filterCutoffId, filterCutoffDial);
+
 }
 
 FIRFilterAudioProcessorEditor::~FIRFilterAudioProcessorEditor()
@@ -25,16 +39,10 @@ FIRFilterAudioProcessorEditor::~FIRFilterAudioProcessorEditor()
 //==============================================================================
 void FIRFilterAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+  
 }
 
 void FIRFilterAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    filterCutoffDial.setBounds(getLocalBounds().reduced(20));
 }
