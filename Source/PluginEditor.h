905858520459d7fac9c5c4b5960b8d2f2e14f7c3
Janith Harinda <jharinda@gmail.com>
2021-05-24 11:21:43 +0530

    Cutoff Slider Created

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -29,5 +29,7 @@ private:
     // access the processor object that created it.
     FIRFilterAudioProcessor& audioProcessor;
 
+    juce::Slider filterCutoffDial;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FIRFilterAudioProcessorEditor)
 };
